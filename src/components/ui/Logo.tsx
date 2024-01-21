941befa5a87543031255281ae7b24458b3544151
Saransh <saranshseth93@gmail.com>
2024-01-21 14:01:58 +1100

    Fix logo

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { FC } from "react";
-import logo from "../../../public/logo.png";
+import logo from "../../../public/Images/logo.png";
 
 interface LogoProps {
   className?: string;
